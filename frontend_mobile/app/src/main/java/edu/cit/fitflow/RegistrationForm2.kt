eafc5702a093d328f4e27e91d90700b51e012e95
kinatulinan <151549802+kinatulinan@users.noreply.github.com>
2025-04-09 00:51:43 +0800

    FitFlow Login, Registration, Dashboard Tweaks

--- frontend_mobile/app/src/main/java/edu/cit/fitflow/RegistrationForm2.kt
+++ frontend_mobile/app/src/main/java/edu/cit/fitflow/RegistrationForm2.kt
@@ -3,18 +3,12 @@ package edu.cit.fitflow
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class RegistrationForm_2 : AppCompatActivity() {
+class RegistrationForm2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_registration_form2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
